2f6b9e7b9cb24c529b6717ee28f929d020ed2999
thomasmarano <thomas.marano2@gmail.com>
2019-02-05 15:26:27 +0000

    Completed FizzBuzz

--- jasmine/spec/FizzBuzzSpec.js
+++ jasmine/spec/FizzBuzzSpec.js
@@ -8,7 +8,33 @@ describe("FizzBuzz", function() {
 
       describe("should return the number where not divisible by 3 or 5", function() {
         it ('prints number for 1', function() {
+              expect(fizzBuzz.play(1)).toEqual(1);
               expect(fizzBuzz.play(8)).toEqual(8);
+              expect(fizzBuzz.play(11)).toEqual(11);
+        });
+    });
+
+    describe("should return fizz where number is divisible by 3", function() {
+        it ('prints fizz for multiples of 3', function() {
+          expect(fizzBuzz.play(3)).toEqual('fizz');
+          expect(fizzBuzz.play(9)).toEqual('fizz');
+          expect(fizzBuzz.play(21)).toEqual('fizz');
+        });
+    });
+
+    describe("should return buzz where number is divisible by 5", function() {
+        it ('prints buzz for multiples of 5', function() {
+            expect(fizzBuzz.play(5)).toEqual('buzz');
+            expect(fizzBuzz.play(25)).toEqual('buzz');
+            expect(fizzBuzz.play(35)).toEqual('buzz');
+        });
+    });
+
+    describe("should return fizzbuzz where number is divisible by 3 & 5", function() {
+        it ('prints fizzbuzz for multiples of 3 & 5', function() {
+            expect(fizzBuzz.play(15)).toEqual('fizzbuzz');
+            expect(fizzBuzz.play(30)).toEqual('fizzbuzz');
+            expect(fizzBuzz.play(45)).toEqual('fizzbuzz');
         });
     });
 });
--- jasmine/src/FizzBuzz.js
+++ jasmine/src/FizzBuzz.js
@@ -2,6 +2,13 @@ function FizzBuzz() {
 };
 
 FizzBuzz.prototype.play = function(number) {
-
-
+    if (number % 3 === 0 && number % 5 === 0){
+        return 'fizzbuzz';
+    } else if (number % 3 === 0) {
+        return 'fizz';
+    } else if (number % 5 === 0) {
+        return 'buzz';
+    } else {
+      return number;
+    }
 };
